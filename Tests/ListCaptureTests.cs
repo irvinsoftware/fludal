0124b729038c67c38d90ae7f7c62f79b5c493032
Nate Irvin <nate.irvin@gmail.com>
2022-06-28 18:22:24 -0600

    adding tests

--- Tests/ListCaptureTests.cs
+++ Tests/ListCaptureTests.cs
@@ -125,7 +125,34 @@ public class ListCaptureTests
     }
     
     [Test]
-    public async Task CapturesEmitAndReturnCode_ForRecord()
+    public async Task CapturesEmitAndReturnCode_ForStructWithProperties()
+    {
+        var actual = await
+                Please.ConnectTo<SqlServer>()
+                    .UsingConnectionString("Data Source=localhost;Integrated Security=SSPI;Initial Catalog=Seal_Test;Application Name=Tests")
+                    .AndExecuteStoredProcedure("dbo.[ComplexList]")
+                    .WithStringParameter("Super", "whatever")
+                    .WithCancellationToken(_cancellation.Token)
+                    .ThenReadAsList<DBModelStruct2>()
+                    .ConfigureAwait(false)
+            ;
+        
+        Assert.AreEqual(9, actual.Code);
+        var actualList = actual.Content;
+        Assert.AreEqual(3, actualList.Count);
+        Assert.IsNull(actualList[0].a);
+        Assert.AreEqual(4, actualList[0].B);
+        Assert.IsNotNull(actualList[0].C);
+        Assert.AreEqual('A', actualList[1].a);
+        Assert.AreEqual(4, actualList[1].B);
+        Assert.IsNull(actualList[1].C);
+        Assert.AreEqual('A', actualList[2].a);
+        Assert.AreEqual(4, actualList[2].B);
+        Assert.IsNotNull(actualList[2].C);
+    }
+    
+    [Test]
+    public async Task CapturesEmitAndReturnCode_ForSimpleRecord()
     {
         var actual = await
                 Please.ConnectTo<SqlServer>()
@@ -150,7 +177,115 @@ public class ListCaptureTests
         Assert.AreEqual(4, actualList[2].B);
         Assert.IsNotNull(actualList[2].c);
     }
-
+    
+    [Test]
+    public async Task CapturesEmitAndReturnCode_ForInitOnlyRecord()
+    {
+        var actual = await
+                Please.ConnectTo<SqlServer>()
+                    .UsingConfiguredConnectionNamed("my_test")
+                    .AndExecuteStoredProcedure("dbo.[ComplexList]")
+                    .WithStringParameter("Super", "whatever")
+                    .WithCancellationToken(_cancellation.Token)
+                    .ThenReadAsList<DBModelRecord3>()
+                    .ConfigureAwait(false)
+            ;
+        
+        Assert.AreEqual(9, actual.Code);
+        var actualList = actual.Content;
+        Assert.AreEqual(3, actualList.Count);
+        Assert.IsNull(actualList[0].A);
+        Assert.AreEqual(4, actualList[0].B);
+        Assert.IsNotNull(actualList[0].C);
+        Assert.AreEqual('A', actualList[1].A);
+        Assert.AreEqual(4, actualList[1].B);
+        Assert.IsNull(actualList[1].C);
+        Assert.AreEqual('A', actualList[2].A);
+        Assert.AreEqual(4, actualList[2].B);
+        Assert.IsNotNull(actualList[2].C);
+    }
+    
+    [Test]
+    public async Task CapturesEmitAndReturnCode_ForRecord()
+    {
+        var actual = await
+                Please.ConnectTo<SqlServer>()
+                    .UsingConfiguredConnectionNamed("my_test")
+                    .AndExecuteStoredProcedure("dbo.[ComplexList]")
+                    .WithStringParameter("Super", "whatever")
+                    .WithCancellationToken(_cancellation.Token)
+                    .ThenReadAsList<DBModelRecord2>()
+                    .ConfigureAwait(false)
+            ;
+        
+        Assert.AreEqual(9, actual.Code);
+        var actualList = actual.Content;
+        Assert.AreEqual(3, actualList.Count);
+        Assert.IsNull(actualList[0].A);
+        Assert.AreEqual(4, actualList[0].b);
+        Assert.IsNotNull(actualList[0].C);
+        Assert.AreEqual('A', actualList[1].A);
+        Assert.AreEqual(4, actualList[1].b);
+        Assert.IsNull(actualList[1].C);
+        Assert.AreEqual('A', actualList[2].A);
+        Assert.AreEqual(4, actualList[2].b);
+        Assert.IsNotNull(actualList[2].C);
+    }
+    
+    [Test]
+    public async Task CapturesEmitAndReturnCode_ForRecordStruct()
+    {
+        var actual = await
+                Please.ConnectTo<SqlServer>()
+                    .UsingConfiguredConnectionNamed("my_test")
+                    .AndExecuteStoredProcedure("dbo.[ComplexList]")
+                    .WithStringParameter("Super", "whatever")
+                    .WithCancellationToken(_cancellation.Token)
+                    .ThenReadAsList<DBModelRecordStruct>()
+                    .ConfigureAwait(false)
+            ;
+        
+        Assert.AreEqual(9, actual.Code);
+        var actualList = actual.Content;
+        Assert.AreEqual(3, actualList.Count);
+        Assert.IsNull(actualList[0].A);
+        Assert.AreEqual(4, actualList[0].B);
+        Assert.IsNotNull(actualList[0].C);
+        Assert.AreEqual('A', actualList[1].A);
+        Assert.AreEqual(4, actualList[1].B);
+        Assert.IsNull(actualList[1].C);
+        Assert.AreEqual('A', actualList[2].A);
+        Assert.AreEqual(4, actualList[2].B);
+        Assert.IsNotNull(actualList[2].C);
+    }
+    
+    [Test]
+    public async Task CapturesEmitAndReturnCode_ForReadonlyRecordStruct()
+    {
+        var actual = await
+                Please.ConnectTo<SqlServer>()
+                    .UsingConfiguredConnectionNamed("my_test")
+                    .AndExecuteStoredProcedure("dbo.[ComplexList]")
+                    .WithStringParameter("Super", "whatever")
+                    .WithCancellationToken(_cancellation.Token)
+                    .ThenReadAsList<DBModelRecordStructLocked>()
+                    .ConfigureAwait(false)
+            ;
+        
+        Assert.AreEqual(9, actual.Code);
+        var actualList = actual.Content;
+        Assert.AreEqual(3, actualList.Count);
+        Assert.IsNull(actualList[0].A);
+        Assert.AreEqual(4, actualList[0].B);
+        Assert.IsNotNull(actualList[0].C);
+        Assert.AreEqual('A', actualList[1].A);
+        Assert.AreEqual(4, actualList[1].B);
+        Assert.IsNull(actualList[1].C);
+        Assert.AreEqual('A', actualList[2].A);
+        Assert.AreEqual(4, actualList[2].B);
+        Assert.IsNotNull(actualList[2].C);
+    }
+    
     private class DBModelClass
     {
         public char? A { get; set; }
@@ -164,6 +299,29 @@ public class ListCaptureTests
         public int B;
         public DateTime? C;
     }
+    
+    private struct DBModelStruct2
+    {
+        public char? a { get; set; }
+        public int B { get; set; }
+        public DateTime? C { get; set; }
+    }
 
     private record DBModelRecord(char? A, int B, DateTime? c);
+    private record struct DBModelRecordStruct(char? A, int B, DateTime? C);
+    private readonly record struct DBModelRecordStructLocked(char? A, int B, DateTime? C);
+
+    private record DBModelRecord2
+    {
+        public char? A { get; set; }
+        public int b { get; set; }
+        public DateTime? C { get; set; }
+    }
+    
+    private record DBModelRecord3
+    {
+        public char? A { get; init; }
+        public int B { get; init; }
+        public DateTime? C { get; init; }
+    }
 }
