3fb8673a8ea144e97f4c541c16b1797fed41232e
Nate Irvin <nate.irvin@gmail.com>
2023-07-28 16:17:13 -0600

    refactoring

---
+++ Irvin.Fludal.SqlClient/SqlMultiResult.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace Irvin.Fludal.SqlClient;
+
+public class SqlMultiPartResult : DbMultiPartResult
+{
+    public SqlMultiPartResult(string connectionAddress, SqlCommand command, CancellationToken cancellationToken) 
+        : base(connectionAddress, command, cancellationToken)
+    {
+    }
+
+    protected override DbConnection CreateConnection(string connectionAddress)
+    {
+        return new SqlConnection(connectionAddress);
+    }
+
+    protected override DbCursor<T> CreateCursor<T>()
+    {
+        return new SqlCursor<T>(_pipeline);
+    }
+}
